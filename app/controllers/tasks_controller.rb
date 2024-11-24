2040c7b9063c3045e9d85e90afc4beeec733f88b
Tasbeeh Naser <142756718+TasbeehNaser99@users.noreply.github.com>
2024-11-24 18:43:01 +0200

    Update tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -45,6 +45,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-      params.require(:task).permit(:title,:description,:data)
+      params.require(:task).permit(:title,:description,:date)
   end
 end
